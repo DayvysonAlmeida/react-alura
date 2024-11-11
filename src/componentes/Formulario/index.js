9fc4e5031c1f4d0c5829854096392aa4fc856178
Dayvyson Almneida <dayvysonalmeida@gmail.com>
2024-11-11 00:28:14 -0300

    criação do botão

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
@@ -12,14 +13,21 @@ const Formulario = () => {
     "Mobile",
     "Inovação e Gestão",
   ]
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('Form foi submetido')
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times}/>
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" />
+        <CampoTexto  label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+        <Botao> Criar Card</Botao>
       </form>
     </section>
   )
